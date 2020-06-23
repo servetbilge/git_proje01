11553be14dbc6a012221a8087d49db2337fd0118
servetbilge <servetbilgebay@gmail.com>
2020-06-23 23:00:51 +0200

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("İlk commit from locak repository");
+		System.out.println("İkinci commit from locak repository");
 
 	}
 
