f8f0ab831ce595a3f5a586c5cca5028caef2f0e9
servetbilge <servetbilgebay@gmail.com>
2020-06-24 15:30:39 +0200

    Feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("İlk commit from locak repository");
 		System.out.println("İkinci commit from locak repository");
+		System.out.println("Yeni brnch oluşturdum feature adında");
 
 	}
 
